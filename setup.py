3dfc041eb2bfbb1105387270ce27a0b0f1901860
Charles Reid <chmreid@ucsc.edu>
2020-01-14 15:59:45 -0800

    remove hca refs in setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ from setuptools import setup, find_packages
 install_requires = [line.rstrip() for line in open(os.path.join(os.path.dirname(__file__), "requirements.txt"))]
 
 setup(
-    name="hca",
+    name="dss",
     version='6.5.1',
-    url='https://github.com/HumanCellAtlas/dcp-cli',
+    url='https://github.com/DataBiosphere/data-store-cli',
     license='Apache Software License',
-    author='Human Cell Atlas contributors',
+    author='Data Store Contributors',
     author_email='akislyuk@chanzuckerberg.com',
-    description='Human Cell Atlas Data Storage System Command Line Interface',
+    description='Data Storage System Command Line Interface',
     long_description=open('README.rst').read(),
     install_requires=install_requires,
     extras_require={
@@ -24,11 +24,11 @@ setup(
     packages=find_packages(exclude=['test']),
     entry_points={
         'console_scripts': [
-            'hca=hca.cli:main'
+            'dss=dss.cli:main'
         ],
     },
     platforms=['MacOS X', 'Posix'],
-    package_data={'hcacli': ['*.json']},
+    package_data={'dsscli': ['*.json']},
     zip_safe=False,
     include_package_data=True,
     test_suite='test',
